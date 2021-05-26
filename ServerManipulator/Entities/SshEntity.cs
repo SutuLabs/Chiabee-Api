1fa610c798aed2e73179e46026c69ee91652ddb8
LiangShuang <liangshuang@sutu.tech>
2021-05-27 05:57:10 +0800

    extract ssh related to individual project

---
+++ ServerManipulator/Entities/SshEntity.cs
@@ -0,0 +1,32 @@
+﻿#nullable enable
+
+namespace WebApi.Models
+{
+    using System.Collections.Generic;
+
+    public class SshEntity
+    {
+        public string? Host { get; set; }
+        public int? Port { get; set; }
+        public string? Username { get; set; }
+        public string? Name { get; set; }
+        public string? PrivateKeyFile { get; set; }
+
+        public static IEnumerable<SshEntity> InheritSshEntities(SshEntity? baseEntity, SshEntity[]? entities)
+        {
+            if (entities == null) yield break;
+            if (baseEntity == null) baseEntity = new SshEntity();
+            foreach (var ent in entities)
+            {
+                yield return new SshEntity
+                {
+                    Host = ent.Host ?? baseEntity.Host,
+                    Port = ent.Port ?? baseEntity.Port,
+                    PrivateKeyFile = ent.PrivateKeyFile ?? baseEntity.PrivateKeyFile,
+                    Username = ent.Username ?? baseEntity.Username,
+                    Name = ent.Name ?? baseEntity.Name,
+                };
+            }
+        }
+    }
+}
--- WebApi/Models/AppSettings.cs
+++ WebApi/Models/AppSettings.cs
@@ -1,10 +1,9 @@
 ﻿#nullable enable
 
-using System.Collections.Generic;
-using System.Linq;
-
 namespace WebApi.Models
 {
+    using System.Linq;
+
     public class AppSettings
     {
         public string? ConnectionString { get; set; }
@@ -16,34 +15,10 @@ namespace WebApi.Models
         public SshEntity? HarvesterDefault { get; set; }
         public SshEntity[]? Harvesters { get; set; }
 
-        public class SshEntity
-        {
-            public string? Host { get; set; }
-            public int? Port { get; set; }
-            public string? Username { get; set; }
-            public string? Name { get; set; }
-            public string? PrivateKeyFile { get; set; }
-        }
+        internal SshEntity[] GetPlotters() => SshEntity.InheritSshEntities(PlotterDefault, Plotters).ToArray();
 
-        internal SshEntity[] GetPlotters() => InheritSshEntities(PlotterDefault, Plotters).ToArray();
-        internal SshEntity[] GetFarmers() => InheritSshEntities(FarmerDefault, Farmers).ToArray();
-        internal SshEntity[] GetHarvesters() => InheritSshEntities(HarvesterDefault, Harvesters).ToArray();
+        internal SshEntity[] GetFarmers() => SshEntity.InheritSshEntities(FarmerDefault, Farmers).ToArray();
 
-        private IEnumerable<SshEntity> InheritSshEntities(SshEntity? baseEntity, SshEntity[]? entities)
-        {
-            if (entities == null) yield break;
-            if (baseEntity == null) baseEntity = new SshEntity();
-            foreach (var ent in entities)
-            {
-                yield return new SshEntity
-                {
-                    Host = ent.Host ?? baseEntity.Host,
-                    Port = ent.Port ?? baseEntity.Port,
-                    PrivateKeyFile = ent.PrivateKeyFile ?? baseEntity.PrivateKeyFile,
-                    Username = ent.Username ?? baseEntity.Username,
-                    Name = ent.Name ?? baseEntity.Name,
-                };
-            }
-        }
+        internal SshEntity[] GetHarvesters() => SshEntity.InheritSshEntities(HarvesterDefault, Harvesters).ToArray();
     }
 }
