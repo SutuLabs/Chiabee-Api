611eee524e434aed7d423962f69a8844203c6184
LiangShuang <liangshuang@sutu.tech>
2021-06-15 19:36:48 +0800

    network speed read receive/send

--- ServerManipulator/ServerCommands/NetworkStatusCommand.cs
+++ ServerManipulator/ServerCommands/NetworkStatusCommand.cs
@@ -9,8 +9,18 @@
             if (!client.EnsureConnected()) return null;
             var duration = 0.1;
             var ethName = "eno1";
-            using var cmd = client.RunCommand(@$"read s1 < /sys/class/net/{ethName}/statistics/rx_bytes; sleep {duration}; read s2 < /sys/class/net/{ethName}/statistics/rx_bytes; echo $(($s2-$s1))");
+            var cmdText =
+                ReadCommand("sr1", "r", ethName) +
+                ReadCommand("st1", "t", ethName) +
+                $"sleep {duration};" +
+                ReadCommand("sr2", "r", ethName) +
+                ReadCommand("st2", "t", ethName) +
+                $"echo $(($sr2-$sr1+$st2-$st1))";
+            using var cmd = client.RunCommand(cmdText);
             return int.TryParse(cmd.Result, out var b) ? (int)(b / duration) : null;
+
+            string ReadCommand(string par, string direction, string ethName) =>
+                $"read {par} < /sys/class/net/{ethName}/statistics/{direction}x_bytes;";
         }
     }
 }
