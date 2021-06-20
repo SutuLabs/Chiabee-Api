c2cd8b15f086b6d90398bc2a62f131ab08c92d08
LiangShuang <liangshuang@sutu.tech>
2021-06-20 11:20:18 +0800

    aggreagate all network interfaces

--- ServerManipulator/ServerCommands/NetworkStatusCommand.cs
+++ ServerManipulator/ServerCommands/NetworkStatusCommand.cs
@@ -7,19 +7,28 @@
         public static int? GetNetworkIoSpeed(this TargetMachine client)
         {
             if (!client.EnsureConnected()) return null;
-            var duration = 0.1;
-            var ethName = "eno1";
-            var cmdText =
+            const decimal duration = 0.1m;
+            var ethBaseName = "eno";
+
+            var sum = 0;
+            for (int i = 0; i < client.Properties.Hosts.Length; i++)
+            {
+                using var cmd = client.RunCommand(GetCmdText($"{ethBaseName}{i + 1}"));
+                var cur = int.TryParse(cmd.Result, out var b) ? (int?)(b / duration) : null;
+                sum += cur ?? 0;
+            }
+
+            return sum;
+
+            static string GetCmdText(string ethName) =>
                 ReadCommand("sr1", "r", ethName) +
                 ReadCommand("st1", "t", ethName) +
                 $"sleep {duration};" +
                 ReadCommand("sr2", "r", ethName) +
                 ReadCommand("st2", "t", ethName) +
                 $"echo $(($sr2-$sr1+$st2-$st1))";
-            using var cmd = client.RunCommand(cmdText);
-            return int.TryParse(cmd.Result, out var b) ? (int)(b / duration) : null;
 
-            string ReadCommand(string par, string direction, string ethName) =>
+            static string ReadCommand(string par, string direction, string ethName) =>
                 $"read {par} < /sys/class/net/{ethName}/statistics/{direction}x_bytes;";
         }
     }
